62ee6a52811015d8ef71a7d7883f73291e59de49
emicon-web <sharmakunal315@gmail.com>
2020-12-08 09:33:26 +0530

    dist

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 require('./config/config');
 require('./startup/prod') (app);
 require('./models/db');
+
+app.use(express.static(path.join(__dirname, './dist/client')));
 require('./startup/routes') (app);
 
 app.listen(process.env.PORT ,() => console.log(`Server Started On Port : ${process.env.PORT}`));
